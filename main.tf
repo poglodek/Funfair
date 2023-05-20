ab9a6ffb9cb8ea66bf11a9ed67e944fd85bf81c8
poglodek <pawelogll@gmail.com>
2023-05-20 15:00:04 +0200

    adding first terrafrom file

---
+++ main.tf
@@ -0,0 +1,20 @@
+#Provides the configuration for the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "3.56.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "funfair_rg"
+  location = "West Europe"
+}
