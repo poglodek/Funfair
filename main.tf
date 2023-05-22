15974133032a8c9ee37de4d1966a08331ba0df2e
poglodek <pawelogll@gmail.com>
2023-05-22 22:19:15 +0200

    refactor tf, adding first database

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "3.56.0"
+      source  = "hashicorp/azurerm"
+      version = "3.56.0"
     }
   }
 }
@@ -15,6 +15,17 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "funfair_rg"
-  location = var.azure_region
+  name     = var.azure_rg_name
+  location = var.azure_region_location
+}
+
+resource "azurerm_postgresql_database" "database" {
+  name                = "${var.azure_postgres_server_name}-users"
+  resource_group_name = var.azure_rg_name
+
+  server_name = "${var.azure_postgres_server_name}-users"
+  charset     = "UTF8"
+  collation   = "English_United States.1252"
+
+
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,15 @@
-variable "azure_region" {
-  type = string
+variable "azure_region_location" {
+  type    = string
   default = "West Europe"
+}
+
+variable "azure_rg_name" {
+  type    = string
+  default = "funfair-rg"
+}
+
+variable "azure_postgres_server_name" {
+  type    = string
+  default = "funfair-postgres-server"
 }
 
