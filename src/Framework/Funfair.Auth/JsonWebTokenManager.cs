65fa8ee43f39f52004d3cfe9e8bd24ae75b5df76
poglodek <pawelogll@gmail.com>
2023-06-04 16:23:24 +0200

    remove using

--- src/Framework/Funfair.Auth/JsonWebTokenManager.cs
+++ src/Framework/Funfair.Auth/JsonWebTokenManager.cs
@@ -1,6 +1,5 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using Disco.Shared.Auth;
 using Microsoft.IdentityModel.Tokens;
 using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
